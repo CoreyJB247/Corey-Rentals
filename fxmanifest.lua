3df59f26547c79d63acf55d5a547aa1def26d7ee
CoreyJB247 <coreybruce1@protonmail.com>
2026-04-16 23:22:55 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,33 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'CoreyJB247'
+description 'solos-rentals edited by CoreyJB247'
+version '1.0.0'
+
+
+shared_scripts {
+    'config.lua',
+    '@ox_lib/init.lua'
+}
+
+client_scripts {
+    'client.lua',
+}
+
+server_script {
+    'server.lua'
+}
+
+escrow_ignore {
+    'config.lua',
+    'client.lua',
+    'server.lua'
+}
+
+dependencies {
+    --'ox_inventory',
+    'ox_lib',
+}
+
+lua54 'yes'
